49b63b32a3ed89ce748ee9a2025a989a2074929a
Evoice-Studio <alexnikishin13@gmail.com>
2026-09-14 21:21:21 +0300

    Сделать проект

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,39 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.ArrayList;
+
+import static ru.yandex.practicum.filmorate.FilmorateApplication.log;
 
 @RestController
+@RequestMapping("/films")
 public class FilmController {
+    private final ArrayList<Film> allFilms = new ArrayList<>();
+
+    @PostMapping()
+    public Film add(@RequestBody Film film) {
+        log.info("Попытка добавить фильм");
+        film.id = allFilms.size() + 1;
+        allFilms.add(film);
+        log.info("Фильм успешно добавлен");
+        return film;
+    }
+
+    @PutMapping()
+    public Film update(@RequestBody Film film) {
+        log.info("Попытка редактирования фильма");
+        if (allFilms.contains(film)) {
+            allFilms.set(allFilms.indexOf(film), film);
+        }
+        log.info("Фильм успешно отредактирован");
+        return film;
+    }
+
+    @GetMapping()
+    public ArrayList<Film> getAll() {
+        log.info("Произошел запрос всех фильмов");
+        return allFilms;
+    }
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,69 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 
-/**
- * Film.
- */
-@Getter
-@Setter
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+
+import static ru.yandex.practicum.filmorate.FilmorateApplication.log;
+
+@Data
 public class Film {
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final LocalDate MINIMUM_DATE = LocalDate.parse("28.12.1895", formatter);
+    public int id;
+    public String name;
+    public String description;
+    public LocalDate releaseDate;
+    public LocalTime duration;
+
+    @JsonCreator
+    public Film(@JsonProperty("name") String name, @JsonProperty("description") String description, @JsonProperty("releaseDate") String releaseDate, @JsonProperty("duration") int duration) throws ValidationException {
+        if ((name == null || name.isEmpty())) {
+            log.error("Ошибка - Название не может быть пустым");
+            log.error("Фильм не добавлен");
+            throw new ValidationException("Название не может быть пустым");
+        }
+        if (description.length() > 200) {
+            log.error("Ошибка - Максимальная длина описания - 200 символов");
+            log.error("Фильм не добавлен");
+            throw new ValidationException("Максимальная длина описания - 200 символов");
+        }
+
+        LocalDate convertRelease = LocalDate.parse(releaseDate, formatter);
+
+        if (convertRelease.isBefore(MINIMUM_DATE)) {
+            log.error("Ошибка - Дата релиза должна быть не раньше 28 декабря 1895 года");
+            log.error("Фильм не добавлен");
+            throw new ValidationException("Дата релиза должна быть не раньше 28 декабря 1895 года");
+        }
+
+        if (duration < 0) {
+            log.error("Ошибка - Продолжительность должна быть положительным числом");
+            log.error("Фильм не добавлен");
+            throw new ValidationException("Продолжительность должна быть положительным числом");
+        }
+
+        this.name = name;
+        this.description = description;
+        this.releaseDate = convertRelease;
+        this.duration = LocalTime.of(duration / 60, duration % 60);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Film film = (Film) o;
+        return id == film.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
